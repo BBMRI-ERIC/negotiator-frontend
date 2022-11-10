4aeb004a2a2fb9055a18d5073f9fca8951188136
Vittorio Meloni <vittorio.meloni@crs4.it>
2022-11-10 14:27:38 +0100

    feat: add vuex store

---
+++ src/store/actions.js
@@ -0,0 +1,21 @@
+export default {
+    getAccessCriteria({ commit }) {
+        const accessCriteria = {
+            "accessCriteria": [
+                {
+                    "name": "description",
+                    "description": "Give a description",
+                    "type": "text",
+                    "required": true
+                },
+                {
+                    "name": "title",
+                    "description": "Give a title",
+                    "type": "text",
+                    "required": true
+                }
+            ]
+        }
+        commit('setCurrentAccessCriteria', accessCriteria)
+    }
+}
