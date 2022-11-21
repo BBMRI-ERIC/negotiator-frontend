160e28ad031462754fb6a717861430cd2f51bc25
Vittorio Meloni <vittorio.meloni@crs4.it>
2022-11-21 11:32:39 +0100

    fix: env substitution (#6)
    * fix: change .env file to be in the dist directory
    
    * fix: env substitution
    
    * Create deploy.yml
    
    * Update docker-ci.yml
    
    * Update deploy.yml
    
    Co-authored-by: Radovan Tomasik <tomasik@mail.muni.cz>

--- src/auth.js
+++ src/auth.js
@@ -26,13 +26,13 @@ const settings = {
     // Where the tokens will be stored
     userStore: new WebStorageStateStore({ store: window.localStorage }),
     // URL to the authentication server (including realm)
-    authority: import.meta.env.VITE_AUTH_URL,
+    authority: 'AUTH_URL_PLACEHOLDER',
     // The name of the client in Keycloak setup for this service
-    client_id: import.meta.env.VITE_CLIENT_ID,
+    client_id: 'CLIENT_ID_PLACEHOLDER',
     // Where to redirect the user to after successful authentication
-    redirect_uri: import.meta.env.VITE_AUTH_REDIRECT_URI,
+    redirect_uri: 'REDIRECT_URI_PLACEHOLDER',
     // Where to redirect the user to after logging the user out
-    post_logout_redirect_uri: import.meta.env.VITE_AUTH_POST_LOGOUT_REDIRECT_URI,
+    post_logout_redirect_uri: 'LOGOUT_URI_PLACEHOLDER',
     // Indicate the the authorization code flow should be used
     response_type: 'code',
     // "openid" tells the server that this client uses oidc for authentication
--- start.sh
+++ start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-sed -i 's|VITE_AUTH_CLIENT_ID_PLACEHOLDER|'"${CLIENT_ID}"'|g' .env
-sed -i 's|VITE_AUTH_URL_PLACEHOLDER|'${AUTH_URL}'|g' .env
-sed -i 's|VITE_AUTH_REDIRECT_URI_PLACEHOLDER|'"${REDIRECT_URI}"'|g' .env
-sed -i 's|VITE_AUTH_POST_LOGOUT_REDIRECT_URI_PLACEHOLDER|'"${POST_LOGOUT_REDIRECT_URI}"'|g' .env
+echo "Replacing env constants in JS"
+for file in dist/assets/*.js*;
+do
+  echo "Processing $file ...";
+
+  sed -i 's|AUTH_URL_PLACEHOLDER|'${AUTH_URL}'|g' $file
+  sed -i 's|CLIENT_ID_PLACEHOLDER|'${CLIENT_ID}'|g' $file
+  sed -i 's|REDIRECT_URI_PLACEHOLDER|'${REDIRECT_URI}'|g' $file
+  sed -i 's|LOGOUT_URI_PLACEHOLDER|'${LOGOUT_URI}'|g' $file
+
+done
+
 http-server dist
