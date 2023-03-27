45b315f3eaf65c4482025e16368d89146621780d
RadovanTomik <tomasik@mail.muni.cz>
2023-03-27 16:22:03 +0200

    Fix proxy target

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,9 +1,6 @@
 import axios from 'axios'
 
 let BASE_API_PATH = '/api/v3'
-if (import.meta.env.DEV){
-    BASE_API_PATH = 'http://localhost:8081/api/v3'
-}
 const ACCESS_CRITERIA_PATH = `${BASE_API_PATH}/access-criteria/`
 const REQUESTS_PATH = `${BASE_API_PATH}/requests`
 const NEGOTIATION_PATH = `${BASE_API_PATH}/negotiations`
