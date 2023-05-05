1c26802242215f9d1e063e7477816b62f4875de0
Vittorio Meloni <vittorio.meloni@crs4.it>
2023-05-05 14:52:33 +0200

    refactor: rename user page

--- src/views/UserPage.vue
+++ src/views/UserPage.vue
@@ -30,6 +30,7 @@ export default {
         ...mapActions(["retrieveNegotiationsByRole"])
     },
     async mounted() {
+        console.log(this.role)
         this.negotiations = await this.retrieveNegotiationsByRole({ role: this.role })
     }
 }
