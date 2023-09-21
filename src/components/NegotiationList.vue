e14ddacfc2b2bd5b70ee6ea21e62c66b6983c777
asulis <alessandro.sulis@crs4.it>
2023-09-21 14:14:43 +0200

    fix: Minor fixes in date sorting

--- src/components/NegotiationList.vue
+++ src/components/NegotiationList.vue
@@ -57,13 +57,6 @@
             </div>
           </div>
         </div>  
-        <!--select name="sorting" @change="sort($event.target.value)" class="form-select form-control">
-          <option>---- Sort by  ----</option>
-          <option value="title">Title</option>
-          <option value="id">Identifier</option>
-          <option value="creationDate">Creation Date </option>
-          <option value="status">Status </option>
-        </select!--> 
         <div class="card mb-2">
           <div class="card-header">
             Filter by status
@@ -174,93 +167,6 @@
   </div>
 </template>
 
-<!--template>
-  <div class="container">
-    <div class="row table-responsive d-flex">
-      <div class="col-md-2">
-        <table
-          id="tableComponent"
-          class="table table-hover"
-        >
-          <thead>
-            <tr class="table-secondary">
-              <th>FILTER</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                All
-                <span class="badge bg-primary rounded-pill float-end">{{
-                  negotiations.length
-                }}</span>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                Accepted
-                <span class="badge bg-primary rounded-pill float-end">0</span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div class="col-md-10">
-        <table
-          id="tableComponent"
-          class="table table-hover negotiation-list-table"
-        >
-          <thead>
-            <tr class="table-secondary">
-              <th
-                v-for="field in headers"
-                :key="field"
-                scope="col"
-                @click="sort(field)"
-              >
-                <div>
-                  <span class="text-uppercase">{{ field }}</span>
-
-                  <span
-                    v-if="sorting[field].order == 'asc'"
-                    class="arrow asc"
-                  />
-                  <span
-                    v-if="sorting[field].order == 'desc'"
-                    class="arrow dsc"
-                  />
-                </div>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr 
-              v-for="item in negotiations"
-              :key="item.id"
-              @click="
-                $router.push({
-                  name: 'negotiation-page',
-                  params: { negotiationId: item.id, userRole: userRole },
-                })
-              "
-            >
-              <td class="col-3">
-                {{ item.id }}
-              </td>
-              <td class="col-5 col-xxl-4">
-                {{ item.payload.project.title }}
-              </td>
-              <td class="col-2">
-                {{ item.status }}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
-</template-->
-
 <script>
 import { ROLES } from "@/config/consts"
 import  NegotiationCard  from "@/components/NegotiationCard.vue"
@@ -339,23 +245,8 @@ export default {
           return eventDate <= endDate
         })     
       }
-
       return filterConditions.length == 0 ? this.negotiations : this.negotiations.filter(item => filterConditions.every(f => f(item)))
 
-
-      //if (this.filters.status.length == 0 && this.filters.daterange.start == "" && this.filters.daterange.end == ""){
-      //  console.log("no filters")
-      //  return this.negotiations //no filters applied 
-      //} 
-      //else if(this.filters.status.length>0 && this.filters.daterange.start == "" && this.filters.daterange.end == ""){
-      // only status filter is applied
-      //  console.log("Filter applied")
-      //  console.log(this.filters)
-      //  console.log(this.negotiations)
-      //return this.negotiations.filter(function(negotiation){
-      // return negotiation.status === "ONGOING" //this.filters["status"]
-      //return this.filters["status"].includes(negotiation.status)
-      //  return this.negotiations.filter(item => this.filters["status"].includes(item.status))
     }
     
 
@@ -402,31 +293,15 @@ export default {
       console.log("called sort by creationDate")
       console.log(a.creationDate)
       console.log(b.creationDate)
-      if (a.creationDate < b.creationDate) {
-        return this.sorting.creationDate.order == "desc" ? a.creationDate.localeCompare(b.creationDate) :  b.creationDate.localeCompare(a.creationDate) 
-      }
-      if (a.creationDate > b.creationDate) {
-        return this.sorting.creationDate.order == "desc" ? b.creationDate.localeCompare(a.creationDate) : a.creationDate.localeCompare(b.creationDate) 
-      }
-      return 0
+      const dateA = new Date(a.creationDate)
+      const dateB = new Date(b.creationDate)
+      return this.sorting.creationDate.order == "desc" ? dateB-dateA : dateA-dateB
 
     },
 
     sort(column){
       console.log(column)
-      //if(this.previuosSortingColumn!= "" &&  this.previuosSortingColumn!= column){
-      // change of sorting column, reset the order of the previous one 
-      //  this.sorting[this.previuosSortingColumn].order = ""
-      //}
-      //if (this.sorting[column].order == "" || this.sorting[column].order == "desc"){
-      //  this.sorting[column].order = "asc"
-      // }
-      //else{
-      //  this.sorting[column].order = "desc"
-      //}
       let sortedNegotiations = this.negotiations
-
-      //this.previuosSortingColumn = column 
       return sortedNegotiations.sort(this.sorting[column]["func"])
     },
     formatDate(date){
@@ -445,20 +320,11 @@ export default {
           console.log("removing value"+filterValue)
           this.filters[filterName].splice(this.filters[filterName].indexOf(filterValue), 1)
         }
-      
-
       }
       else {
         this.filters.filterName = filterValue
       }
-      
       console.log(this.filters)
-      //let filteredNegotiations = this.negotiations.filter(function(negotiation){
-      //  return negotiation.status === "ONGOING" //this.filters["status"]
-      //})
-      //console.log("Filtered")
-      //console.log(filteredNegotiations)
-      //return filteredNegotiations
     }
   }
   
