34b0e10e5330ddf4cf00075a5e44e952325ba6fb
Vittorio Meloni <vittorio.meloni@crs4.it>
2023-10-04 13:44:13 +0200

    fix: adds missing files

---
+++ src/components/modals/FeedbackModal.vue
@@ -0,0 +1,58 @@
+<template> 
+  <NegotiatorModal
+    :id="id"
+    :title="title"
+    :fade="fade"
+  >
+    <template #body>
+      <p>
+        {{ text }}
+      </p>
+    </template>
+    <template #footer>
+      <button
+        type="button"
+        class="btn btn-info"
+        data-bs-dismiss="modal"
+        @click="emitDismiss"
+      >
+        {{ dismissButtonText }}
+      </button>
+    </template>
+  </NegotiatorModal>
+</template>
+<script>
+import NegotiatorModal from "./NegotiatorModal.vue"
+
+export default {
+  components: { NegotiatorModal },
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    fade: {
+      type: Boolean,
+      default: false
+    },
+    dismissButtonText: {
+      type: String,
+      default: "OK"
+    }
+  },
+  emits: ["dismiss"],
+  methods: {
+    emitDismiss() {
+      this.$emit("dismiss")
+    }
+  }
+}
+</script>
--- src/components/modals/NegotiatorModal.vue
+++ src/components/modals/NegotiatorModal.vue
@@ -1,20 +1,21 @@
 <template>
   <div
     :id="id"
-    class="modal fade"
+    class="modal"
+    :class="{ fade: fade }"
     tabindex="-1"
-    aria-labelledby="{{id}}Label"
+    :aria-labelledby="`${id}Label`"
     aria-hidden="true"
   >
     <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
+      <div class="modal-content text-center">
         <h4 class="modal-header">
           {{ title }}
         </h4>
         <div class="modal-body">
           <slot name="body" />
         </div>
-        <div class="modal-footer justify-content-center bg-primary">
+        <div class="modal-footer justify-content-center bg-light">
           <slot name="footer" />
         </div>
       </div>
@@ -36,12 +37,11 @@ export default {
     text: {
       type: String,
       required: true
-    }
-  },
-  emits: ["confirm"],
-  methods: {
-    emitConfirm () {
-      this.$emit("confirm")
+    },
+    fade: {
+      type: Boolean,
+      required: false,
+      default: true
     }
   }
 }
