34f6ef5824dc16ead871bb5b880de1172b62df20
Milos <tmilost>
2024-01-16 10:45:42 +0100

    redesign log-in and fix resizability

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -7,55 +7,63 @@
       <div
         class="col-sm-10"
       >
-        <div class="card shadow-lg pt-5 p-3">
+        <div class="card py-5 p-3">
+          <div class="col-10 col-md-4 align-self-center">
+            <img
+              src="../assets/images/bbmri-logo.png"
+              class="img-fluid mt-4 mb-2"
+              style="min-width: 50px;"
+              alt="bbmri-logo"
+            >
+          </div>
           <h1
-            class="text-center card-title text-secondary fw-bold mb-4"
+            class="text-center card-title text-logo-grey fw-bold mb-5"
           >
-            <b>BBMRI-ERIC® NEGOTIATOR</b>
+            <b>NEGOTIATOR</b>
           </h1>
           <div class="card-body">
-            <h2 class="card-subtitle text-center text-primary fw-bold pb-5">
+            <h4 class="card-subtitle text-center text-primary fw-bold pb-2">
               Choose how to log in
-            </h2>
+            </h4>
             <div
-              class="d-grid mx-3"
+              class="d-grid mx-3 mb-5"
             >
               <button
                 class="btn btn-outline-light"
                 @click.stop.prevent="authenticateOidc"
               >
                 <img
-                  width="34"
-                  height="28"
-                  class="float-start border-end border-2 pe-2"
+                  width="28"
+                  height="23"
+                  class="float-center mb-1 pe-2"
                   src="../assets/images/ls-aai-logo.png"
                   alt="icon"
                 >
-                <span class="fw-bold align-self-center pe-4">
+                <span class="align-self-center fw-boldtext-logo-grey pe-4">
                   Life Science Login</span>
               </button>
             </div>
           </div>
-          <img
-            src="../assets/images/bbmri-logo.png"
-            class="w-25 align-self-center mt-4 me-2"
-            style="min-width: 50px;"
-            alt="bbmri-logo"
-          >
         </div>
         <div
-          class="text-center mt-5 mb-2"
+          class="text-center mt-2 mb-2"
         >
           Not familiar with LS Login? Visit their <a
             target="_blank"
             href="https://lifescience-ri.eu/ls-login.html"
           >website</a>.
         </div>
-        <div class="text-center mb-5">
+        <div class="text-center mb-2">
           Need help? <a href="mailto:negotiator@helpdesk.bbmri-eric.eu">Contact us</a>.
         </div>
+        <div class="text-center col mb-2">
+            <i class="bi bi-github" />
+            <a href="https://github.com/BBMRI-ERIC/negotiator-v3-frontend"> GitHub</a>
+        </div>
+        <div class="text-center mb-5">
+          <p>&copy; 2024 BBMRI-ERIC</p>
+        </div>
       </div>
-      <div class="col-1" />
     </div>
   </div>
 </template>
