121f65c4c1f803d4fa29a321eb99887d8f7647c7
Milos <tmilost>
2024-02-06 09:53:58 +0100

    perserve Vuex state when token expire

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,6 @@ import Vuex from "vuex"
 import { vuexOidcCreateStoreModule } from "vuex-oidc"
 import oidcSettings from "../config/oidc"
 
-
 import state from "./state"
 import mutations from "./mutations"
 import getters from "./getters"
@@ -11,7 +10,20 @@ import actions from "./actions"
 
 export default new Vuex.Store({
   modules: {
-    oidc: vuexOidcCreateStoreModule(oidcSettings)
+    oidc: vuexOidcCreateStoreModule(oidcSettings,
+    // Optional OIDC store settings
+    {  removeUserWhenTokensExpire: false },
+    // Optional OIDC event listeners
+    // {
+    //   userLoaded: (user) => console.log('OIDC user is loaded:', user),
+    //   userUnloaded: () => console.log('OIDC user is unloaded'),
+    //   accessTokenExpiring: () => console.log('Access token will expire'),
+    //   accessTokenExpired: () => console.log('Access token did expire'),
+    //   silentRenewError: () => console.log('OIDC user is unloaded'),
+    //   userSignedOut: () => console.log('OIDC user is signed out'),
+    // }
+      ),
+    
   },
   state,
   mutations,
