2b03ffd27d6276925ba05d8704d4a133704ddf08
Milos <tmilost>
2024-02-07 17:18:14 +0100

    remove commented code

--- src/store/index.js
+++ src/store/index.js
@@ -10,20 +10,11 @@ import actions from "./actions"
 
 export default new Vuex.Store({
   modules: {
-    oidc: vuexOidcCreateStoreModule(oidcSettings,
-    // Optional OIDC store settings
-    {  removeUserWhenTokensExpire: false },
-    // Optional OIDC event listeners
-    // {
-    //   userLoaded: (user) => console.log('OIDC user is loaded:', user),
-    //   userUnloaded: () => console.log('OIDC user is unloaded'),
-    //   accessTokenExpiring: () => console.log('Access token will expire'),
-    //   accessTokenExpired: () => console.log('Access token did expire'),
-    //   silentRenewError: () => console.log('OIDC user is unloaded'),
-    //   userSignedOut: () => console.log('OIDC user is signed out'),
-    // }
-      ),
-    
+    oidc: vuexOidcCreateStoreModule(
+      oidcSettings,
+      // Optional OIDC store settings
+      { removeUserWhenTokensExpire: false },
+    ),
   },
   state,
   mutations,
