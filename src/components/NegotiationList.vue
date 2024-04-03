4c149d5f6adc44d894942be015090b1c198a0d82
Milos <tmilost>
2024-04-03 14:26:34 +0200

    add url params

--- src/components/NegotiationList.vue
+++ src/components/NegotiationList.vue
@@ -3,159 +3,6 @@
     v-if="!loading"
     class="container"
   >
-    <div class="d-flex flex-row flex-wrap justify-content-between">
-      <div class="d-flex flex-row gap-2 my-2 mx-auto mx-md-0">
-        <div class="sort-by">
-          <button
-            class="btn btn-sm btn-outline-sort-filter-button-outline dropdown-toggle"
-            :class="filtersSortData.sortBy ? 'btn-primary': ''"
-            type="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Sort by
-          </button>
-          <ul
-            class="dropdown-menu"
-            aria-labelledby="dropdownSortingButton"
-            role="menu"
-          >
-            <div
-              v-for="(sort, index) in sortBy"
-              :key="index"
-              class="form-check mx-2 my-2 text-sort-filter-dropdown-text"
-            >
-              <input
-                :id="index"
-                v-model="filtersSortData.sortBy"
-                class="form-check-input"
-                type="radio"
-                name="sort"
-                :value="sort.value"
-                :checked="isChecked(index)"
-                @change="emitFilterSortData"
-              >
-              <label
-                class="form-check-label"
-              >
-                {{ sort.label }}
-              </label>
-            </div>
-          </ul>
-        </div>
-        <div class="filter-by-status">
-          <button
-            class="btn btn-sm btn-outline-sort-filter-button-outline dropdown-toggle"
-            :class="filtersSortData.status.length > 0 ? 'btn-primary':''"
-            type="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Filter by status
-          </button>
-          <ul
-            class="dropdown-menu"
-            aria-labelledby="dropdownSortingButton"
-            role="menu"
-          >
-            <div
-              v-for="(status, index) in filtersStatus"
-              :key="index"
-              class="form-check mx-2 my-2"
-            >
-              <input
-                id="submitted"
-                v-model="filtersSortData.status"
-                class="form-check-input"
-                type="checkbox"
-                :value="status.value"
-                @change="emitFilterSortData"
-              >
-              <label
-                class="form-check-label"
-                for="submitted"
-              >
-                {{ status.label }}
-              </label>
-            </div>
-          </ul>
-        </div>
-
-        <div class="filter-by-date">
-          <button
-            class="btn btn-sm btn-outline-sort-filter-button-outline dropdown-toggle"
-            :class="filtersSortData.dateStart !== '' || filtersSortData.dateEnd !== '' ? 'btn-primary':''"
-
-            type="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Filter by date
-          </button>
-          <ul
-            class="dropdown-menu"
-            aria-labelledby="dropdownMenuButton1"
-          >
-            <div class="mx-2 my-2 dropdown-contents text-sort-filter-dropdown-text">
-              <div class="d-flex align-items-center mb-2">
-                <label
-                  class="pe-2 w-25"
-                  for="startDate"
-                >Start:</label>
-                <input
-                  id="startDate"
-                  v-model="filtersSortData.dateStart"
-                  class="form-control text-sort-filter-dropdown-text"
-                  type="date"
-                  @input="emitFilterSortData"
-                >
-              </div>
-              <div class="d-flex align-items-center">
-                <label
-                  for="endDate"
-                  class="pe-2 w-25"
-                >End:</label>
-                <input
-                  id="endDate"
-                  v-model="filtersSortData.dateEnd"
-                  class="form-control text-sort-filter-dropdown-text"
-                  type="date"
-                  @input="emitFilterSortData"
-                >
-              </div>
-            </div>
-          </ul>
-        </div>
-
-        <button
-          class="btn btn-sm btn-outline-sort-filter-button-outline"
-          type="button"
-          @click="changeSortDirection(); emitFilterSortData();"
-        >
-          <i
-            v-if="filtersSortData.sortDirection === 'DESC'"
-            class="bi bi-sort-down"
-          />
-          <i
-            v-if="filtersSortData.sortDirection === 'ASC'"
-            class="bi bi-sort-up"
-          />
-        </button>
-      </div>
-
-      <div class="my-2 ms-auto">
-        <button
-          type="button"
-          class="btn btn-sm btn-outline-sort-filter-clear-button-outline"
-          @click="clearAllFilters()"
-        >
-          <i class="bi bi-x-circle" />
-
-          Clear all filters
-        </button>
-      </div>
-    </div>
-
     <NewRequestButton />
     <div class="pt-1">
       <div class="row row-cols-2 d-grid-row mt-5 ">
@@ -384,7 +231,6 @@ export default {
       }
     }
   },
-  emits: ["filtersSortData"],
   data () {
     return {
       availableRoles: ROLES,
@@ -401,18 +247,10 @@ export default {
         { value: "title", label: "Title" },
         { value: "creationDate", label: "Creation Date" },
         { value: "currentState", label: "Current State" }
-      ],
-      filtersSortData: {
-        status: [],
-        dateStart: "",
-        dateEnd: "",
-        sortBy: "",
-        sortDirection: "DESC"
-      }
+      ]
     }
   },
   computed: {
-
     loading () {
       return this.negotiations === undefined
     },
@@ -428,30 +266,8 @@ export default {
     ...mapActions([
       "setSavedNegotiationsView"
     ]),
-    emitFilterSortData () {
-      this.$emit("filtersSortData", this.filtersSortData)
-    },
-    changeSortDirection () {
-      if (this.filtersSortData.sortDirection === "DESC") {
-        this.filtersSortData.sortDirection = "ASC"
-      } else {
-        this.filtersSortData.sortDirection = "DESC"
-      }
-    },
     formatDate (date) {
       return moment(date).format("YYYY/MM/DD HH:mm")
-    },
-    clearAllFilters () {
-      this.filtersSortData = {
-        status: [],
-        dateStart: "",
-        dateEnd: "",
-        sortBy: "",
-        sortDirection: "DESC"
-      }
-    },
-    isChecked (value) {
-      return "this.sortBy.sortColumn === value"
     },
     transformStatus (string) {
       return transformStatus(string)
