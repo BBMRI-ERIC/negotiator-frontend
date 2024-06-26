ec786304c9e1b03f166c5f3769aa5f6cfb013fda
Milos <tmilos94@gmail.com>
2024-06-26 15:39:28 +0200

    feat: add search parameters and collection id

---
+++ src/components/CopyTextButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    type="button"
+    class="btn btn-sm btn-muted"
+    @click="copyTextToClipboard(text)"
+  >
+    <span class="bi-copy" />
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+    default: ""
+  }
+})
+
+function copyTextToClipboard (text) {
+  navigator.clipboard.writeText(text)
+}
+</script>
