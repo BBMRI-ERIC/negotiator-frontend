a3431b5321621761e0d895fac4eb8a1a9f22bee0
Milos <tmilos94@gmail.com>
2024-07-04 17:59:59 +0200

    feat: add feature to set featureflags from URL  params

--- src/config/featureFlags.js
+++ src/config/featureFlags.js
@@ -12,6 +12,37 @@ const prodSettings = {
 
 let allFeatureFlags
 
+function getSearchParameters () {
+  const prmstr = window.location.search.substr(1)
+  return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {}
+}
+
+function transformToAssocArray (prmstr) {
+  const params = {}
+  const prmarr = prmstr.split("&")
+  for (let i = 0; i < prmarr.length; i++) {
+    const tmparr = prmarr[i].split("=")
+    params[tmparr[0]] = tmparr[1]
+  }
+  return params
+}
+
+const params = getSearchParameters()
+
+function setFeatureFlagsFromParams () {
+  for (const key in params) {
+    if (devSettings[key] || prodSettings[key]) {
+      if (import.meta.env.DEV) {
+        devSettings[key] = params[key]
+      } else {
+        prodSettings[key] = params[key]
+      }
+    }
+  }
+}
+
+setFeatureFlagsFromParams()
+
 if (import.meta.env.DEV) {
   allFeatureFlags = devSettings
 } else {
