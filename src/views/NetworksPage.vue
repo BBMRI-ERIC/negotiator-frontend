e99d97be9108e5433278ffb1a040880eb316053a
Radovan Tomasik <tomasik.radovan@gmail.com>
2024-09-04 10:14:51 +0200

    feat: networks page

--- src/views/NetworksPage.vue
+++ src/views/NetworksPage.vue
@@ -36,6 +36,74 @@
           </ul>
         </div>
       </div>
+      <h4 class="mb-4">
+        <i class="bi bi-graph-up" />
+        Insights:
+      </h4>
+      <div class="card">
+        <div class="card-body">
+          <h4 class="card-title">
+            Statistics Overview
+          </h4>
+
+          <!-- Progress Bar -->
+          <div class="progress mb-4">
+            <div
+              v-for="(stat, key) in stats"
+              :key="key"
+              :class="['progress-bar', stat.color]"
+              :style="{ width: (stat.value / total * 100) + '%' }"
+            >
+              {{ stat.value }}
+            </div>
+          </div>
+          <div class="row text-center">
+            <div
+              v-for="(stat, key) in stats"
+              :key="key"
+              class="col-md-4"
+            >
+              <div class="stat-box">
+                <h5>{{ (stat.value / total * 100).toFixed(0) }}</h5>
+                <p class="text-muted">
+                  {{ stat.label }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex align-items-center my-4">
+        <hr class="flex-grow-1">
+        <span class="mx-3 text-muted"> <i class="bi bi-circle" /> Latest Negotiations</span>
+        <hr class="flex-grow-1">
+      </div>
+      <ul class="list-unstyled">
+        <li
+          v-for="negotiation in negotiations"
+          :key="negotiation.id"
+        >
+          <div class="d-flex align-items-center">
+            <span
+              class="badge"
+              :class="getBadgeColor(negotiation.status)"
+              style="width: 120px;"
+            >
+              <i
+                :class="getBadgeIcon(negotiation.status)"
+                class="px-1"
+              />
+              {{ transformStatus(negotiation.status) }}
+            </span>
+            <h5 class="mb-0 ms-1">
+              {{ negotiation ? negotiation.payload.project.title?.toUpperCase() : "" }}
+            </h5>
+          </div>
+          <p class="text-muted small">
+            #{{ negotiation.id }} &bullet; Created on: {{ new Date(negotiation.creationDate).toLocaleDateString() }}
+          </p>
+        </li>
+      </ul>
     </div>
   </div>
   <LoadingSpinner v-else: />
@@ -47,6 +115,7 @@ import { useNetworksPageStore } from "@/store/networksPage"
 import LoadingSpinner from "@/components/LoadingSpinner.vue"
 import { useRoute } from "vue-router"
 import { useUserStore } from "@/store/user"
+import { getBadgeColor, getBadgeIcon, transformStatus } from "@/composables/utils"
 
 const props = defineProps({
   networkId: {
@@ -58,18 +127,32 @@ const route = useRoute()
 const userStore = useUserStore()
 const networksPageStore = useNetworksPageStore()
 const network = ref(undefined)
+const negotiations = ref(undefined)
 const loading = computed(() => {
-  return network.value === undefined
+  return network.value === undefined && negotiations.value === undefined
+})
+const stats = ref({
+  in_progress: { value: 50, label: "In Progress", color: "bg-success" },
+  concluded: { value: 30, label: "Concluded", color: "bg-info" },
+  abandoned: { value: 20, label: "Abandoned", color: "bg-danger" }
+})
+const total = computed(() => {
+  return Object.values(stats.value).reduce((sum, stat) => sum + stat.value, 0)
 })
 onMounted(async () => {
   if (Object.keys(userStore.userInfo).length === 0) {
     await userStore.retrieveUser()
   }
   loadNetworkInfo(props.networkId)
+  retrieveLatestNegotiations(props.networkId)
 })
 
 async function loadNetworkInfo (networkId) {
   network.value = await networksPageStore.retrieveNetwork(networkId)
+}
+async function retrieveLatestNegotiations (networkId) {
+  const response = await networksPageStore.retrieveNetworkNegotiations(networkId)
+  negotiations.value = response._embedded.negotiations
 }
 </script>
 <style>
@@ -138,5 +221,15 @@ async function loadNetworkInfo (networkId) {
 }
 .organization-info ul li svg {
   margin-right: 0.5rem;
+}
+.stat-box {
+  background-color: #f8f9fa;
+  border: 1px solid #dee2e6;
+  border-radius: 0.25rem;
+  padding: 20px;
+  text-align: center;
+}
+.stat-box h5 {
+  margin-bottom: 0;
 }
 </style>
