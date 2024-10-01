302e018cd7b6d0330cc65f405ce29a92c489a05f
Milos <tmilos94@gmail.com>
2024-10-01 15:48:33 +0200

    feat: Added feature flag for LS login, service down warning

--- start.sh
+++ start.sh
@@ -16,6 +16,8 @@ do
   sed -i 's|ACTIVE_LOGOS_PLACEHOLDER|'${ACTIVE_LOGOS:-bbmri}'|g' $file
   sed -i 's|FEATURE_FLAG_FAQPAGE_PLACEHOLDER|'${FEATURE_FLAG_FAQPAGE:-false}'|g' $file
   sed -i 's|FEATURE_FLAG_NETWORKS_PLACEHOLDER|'${FEATURE_FLAG_NETWORKS:-false}'|g' $file
+  sed -i 's|FEATURE_FLAG_LS_LOGIN_DOWN_WARNING_PLACEHOLDER|'${FEATURE_FLAG_LS_LOGIN_DOWN_WARNING:-false}'|g' $file
+  sed -i 's|FEATURE_FLAG_LS_LOGIN_DOWN_WARNING_TEXT_PLACEHOLDER|'${FEATURE_FLAG_LS_LOGIN_DOWN_WARNING_TEXT:-"We're sorry, but we're having some technical difficulties. Please try again later."}'|g' $file
   sed -i 's|FEATURE_FLAG_VUETOUR_PLACEHOLDER|'${FEATURE_FLAG_VUETOUR:-false}'|g' $file
   sed -i 's|FEATURE_FLAG_NOTIFICATIONS|'${FEATURE_FLAG_NOTIFICATIONS:-false}'|g' $file
   sed -i 's|AUTH_MANAGEMENT_LINK_PLACEHOLDER|'${AUTH_MANAGEMENT_LINK:-none}'|g' $file
