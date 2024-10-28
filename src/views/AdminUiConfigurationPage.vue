7b2836ad9fc69a683e5c0488d85c6c27ab856697
Milos <tmilos94@gmail.com>
2024-10-28 18:14:23 +0100

    feat: add home page login ui-config

--- src/views/AdminUiConfigurationPage.vue
+++ src/views/AdminUiConfigurationPage.vue
@@ -1,26 +1,55 @@
 <template>
-     <div class="navbar mb-2 flex row">
+    <confirmation-modal
+      id="saveModal"
+      title="Are you sure you want to save?"
+      text="Confirming, you will change ui-configuration."
+      @confirm="save()"
+    />
+    <div class="ui-configuration">
+     <div class="nav-bar mb-2">
         <div class="mb-3 text-left fw-bold h3">
             Navbar Settings
+            <button class="btn btn-sm btn-light" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
+                <i class="bi bi-database-fill-gear"/> Click to Edit
+            </button>
         </div>
-        <div class="navbarConfig row">
+        <div class="navbarConfig row collape show" id="collapse1">
             <UiConfigurationSetting v-model="uiConfiguration.navbar" />
+        </div>
+    </div>
+    <div class="login mb-2">
+        <div class="mb-3 text-left fw-bold h3">
+            Login page Settings
+            <button class="btn btn-sm btn-light" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
+                <i class="bi bi-database-fill-gear"/> Click to Edit
+            </button>
+        </div>
+        <div class="loginConfig row collapse" id="collapse2">
+            <UiConfigurationSetting v-model="uiConfiguration.login" />
+            <HomePage :isUiConfigActive="true" />
         </div>
     </div>
     <div class="footer mb-2">
         <div class="mb-3 text-left fw-bold h3">
             Footer Settings
+            <button class="btn btn-sm btn-light" type="button" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
+                <i class="bi bi-database-fill-gear"/> Click to Edit
+            </button>
         </div>
-        <div class="footerConfig row">
+        <div class="footerConfig row collapse" id="collapse3">
             <UiConfigurationSetting v-model="uiConfiguration.footer" />
         </div>
     </div>
-    <div>
-        <button class="btn btn-sm bg-primary sm my-2">
-            <span class="text-white" v-on:click="retriveUiConfiguration()">Restart Settings to default</span>
+</div>
+    <div class="sticky-bottom pb-3">
+        <button class="btn btn-sm bg-primary sm my-2" v-on:click="restartSettings()">
+            <i class="bi bi-arrow-clockwise text-white">Restart to default</i>
         </button>
-        <button class="btn btn-sm bg-primary sm my-2 float-end">
-            <span class="text-white">Save Changes</span>
+        <button class="btn btn-sm bg-primary sm my-2 float-end"     
+            data-bs-toggle="modal"
+            data-bs-target="#saveModal"
+        >
+        <i class="bi bi-floppy text-white"> Save Changes </i>
         </button>
     </div>
 </template>
@@ -29,10 +58,28 @@
 import { computed } from "vue";
 import UiConfigurationSetting from '../components/UiConfigurationSetting.vue'
 import { useUiConfiguration } from '../store/uiConfiguration.js'
+import HomePage from "../views/HomePage.vue"
+
+import ConfirmationModal from "@/components/modals/ConfirmationModal.vue"
 
 const uiConfigurationStore = useUiConfiguration()
 
 const uiConfiguration = computed(() => {
   return uiConfigurationStore.uiConfiguration
 })
+
+function restartSettings() {
+    uiConfigurationStore.retrieveUiConfiguration()
+}
+
+function save() {
+    uiConfigurationStore.updateUiConfiguration(uiConfiguration.value)
+}
+
 </script>
+
+<style scoped>
+.ui-configuration {
+    min-height: 70vh;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,8 @@
 <template>
   <div
-    v-if="!oidcIsAuthenticated"
-    class="container-fluid d-flex justify-content-center align-items-center vh-100  mt-5"
+    v-if="!oidcIsAuthenticated || isUiConfigActive"
+    class="container-fluid d-flex justify-content-center align-items-center vh-100"
+    :class="isUiConfigActive ? '' : 'mt-5'"
   >
     <div class="row">
       <div class="col-1" />
@@ -11,19 +12,21 @@
         <div class="card py-5 p-3">
           <div class="col-10 col-md-4 align-self-center">
             <img
-              :src="logoSrc"
+              :src="returnLogoSrc"
               class="img-fluid mt-4 mb-2"
               style="min-width: 50px;"
               alt="home-page-logo"
             >
           </div>
           <h1
-            class="text-center card-title text-login-tittle-text fw-bold mb-5"
+            class="text-center card-title fw-bold mb-5"
+            :style="{'color':uiConfiguration?.loginNegotiatorTextColor}"
           >
             <b>NEGOTIATOR</b>
           </h1>
           <div class="card-body">
-            <h4 class="card-subtitle text-center text-primary fw-bold pb-2">
+            <h4 class="card-subtitle text-center fw-bold pb-2"
+            :style="{'color':uiConfiguration?.loginTextColor}">
               Choose how to log in
             </h4>
             <div
@@ -40,61 +43,69 @@
                   src="../assets/images/ls-aai-logo.png"
                   alt="icon"
                 >
-                <span class="align-self-center  text-primary pe-4">
+                <span class="align-self-center pe-4"
+                :style="{'color':uiConfiguration?.loginTextColor}">
                   Life Science Login</span>
               </button>
             </div>
           </div>
         </div>
         <div
-          class="text-center text-primary-text mt-2 mb-2"
-        >
+          class="text-center mt-2 mb-2"
+          :style="{'color':uiConfiguration?.loginLinksTextColor}">
           Not familiar with LS Login? Visit their <a
-            class="text-primary"
+            :style="{'color':uiConfiguration?.loginLinksColor}"
             target="_blank"
             href="https://lifescience-ri.eu/ls-login.html"
           >Website</a>.
         </div>
-        <div class="text-center text-primary-text col mb-2">
+        <div class="text-center col mb-2">
           <i class="bi bi-github me-1" />
-          <a href="https://github.com/BBMRI-ERIC/negotiator-v3-frontend">GitHub UI</a>
+          <a href="https://github.com/BBMRI-ERIC/negotiator-v3-frontend" :style="{'color':uiConfiguration?.loginLinksColor}">GitHub UI</a>
           <span class="ms-2">
             <i class="bi bi-github me-1" />
-            <a href="https://github.com/BBMRI-ERIC/negotiator">GitHub Application</a>
+            <a href="https://github.com/BBMRI-ERIC/negotiator" :style="{'color':uiConfiguration?.loginLinksColor}">GitHub Application</a>
           </span>
         </div>
         <div
-          class="text-center text-primary-text mt-2 mb-2"
+          class="text-center mt-2 mb-2"
         >
           <a
-            href="/api/swagger-ui/index.html"
-          > <i class="bi bi-braces-asterisk text-primary-text" />
+            href="/api/swagger-ui/index.html" 
+            :style="{'color':uiConfiguration?.loginLinksColor}"
+          > 
+          <i class="bi bi-braces-asterisk text-primary-text" />
             API
           </a>
           <a
             href="https://status.bbmri-eric.eu/"
             class="ps-2"
-          >  <i class="bi bi-check-circle text-primary-text" />
+            :style="{'color':uiConfiguration?.loginLinksColor}"
+          >  
+          <i class="bi bi-check-circle text-primary-text" />
             BBMRI-ERIC Status page
           </a>
         </div>
-        <div class="text-center text-primary-text mb-2">
+        <div class="text-center mb-2"
+        :style="{'color':uiConfiguration?.loginLinksTextColor}">
           Need help? <a
-            class="text-primary"
+           :style="{'color':uiConfiguration?.loginLinksColor}"
             href="mailto:negotiator@helpdesk.bbmri-eric.eu"
           >Contact us</a>.
         </div>
-        <div class="text-center text-primary-text col mb-2">
+        <div class="text-center col mb-2">
           <i class="bi bi-github" />
           <a
-            class="text-primary"
+          :style="{'color':uiConfiguration?.loginLinksColor}"
             href="https://github.com/BBMRI-ERIC/negotiator-v3-frontend"
           > GitHub</a>
         </div>
-        <div class="text-center text-light">
+        <div class="text-center"
+        :style="{'color':uiConfiguration?.loginLinksTextColor, 'opacity': 0.5}">
           UI version: <span class="pe-2">{{ gitTag }}</span>Server version: <span>{{ backendVersion }}</span>
         </div>
-        <div class="text-center text-primary-text mb-5">
+        <div class="text-center mb-5"
+        :style="{'color':uiConfiguration?.loginLinksTextColor}">
           <p>&copy; 2024 BBMRI-ERIC</p>
         </div>
       </div>
@@ -111,24 +122,47 @@ import canservLogo from "../assets/images/canserv/home-canserv.png"
 import { useRouter } from "vue-router"
 import { useActuatorInfoStore } from "../store/actuatorInfo.js"
 import { useOidcStore } from "../store/oidc.js"
+import { useUiConfiguration } from '../store/uiConfiguration.js'
+
+const props = defineProps({
+  isUiConfigActive: {
+    type: Boolean,
+    default: false
+  }
+})
 
 const viteGitTag = import.meta.env.VITE_GIT_TAG
 
+const uiConfigurationStore = useUiConfiguration()
 const actuatorInfoStore = useActuatorInfoStore()
 
 const router = useRouter()
 
-const logoSrc = activeTheme.activeLogosFiles === "eucaim" ? eucaimLogo : (activeTheme.activeLogosFiles === "canserv" ? canservLogo : bbmriLogo)
 const gitTag = ref(viteGitTag)
 const backendVersion = ref("")
 const oidcStore = useOidcStore()
+
+const uiConfiguration = computed(() => {
+  return uiConfigurationStore.uiConfiguration?.login
+})
 
 const oidcIsAuthenticated = computed(() => {
   return oidcStore.oidcIsAuthenticated
 })
 
+const returnLogoSrc = computed(() => {
+  if(uiConfiguration.value?.loginLogoUrl === 'bbmri'){
+    return bbmriLogo
+  } else if(uiConfiguration.value?.loginLogoUrl === 'canserv'){
+    return canservLogo
+  } else if(uiConfiguration.value?.loginLogoUrl === 'eucaim'){
+    return eucaimLogo
+  }
+  return uiConfiguration.value?.loginLogoUrl
+})
+
 onBeforeMount(() => {
-  if (oidcIsAuthenticated.value) {
+  if (oidcIsAuthenticated.value && !props.isUiConfigActive) {
     router.push("/researcher")
   }
   actuatorInfoStore.retrieveBackendActuatorInfo().then(() => {
