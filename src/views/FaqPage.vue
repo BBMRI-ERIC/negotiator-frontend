b80a9a96e6af2f8762877b8ec1be9b2ada2976e1
Milos <tmilos94@gmail.com>
2024-10-31 18:33:56 +0100

    feat: add setting for  faq page

--- src/views/FaqPage.vue
+++ src/views/FaqPage.vue
@@ -1,22 +1,23 @@
 <template>
-  <h1 class="text-center mb-5">
+  <h1 class="text-center mb-5" :style="{'color': uiConfiguration?.primaryTextColor}">
     Support
   </h1>
   <div class="row row-cols-1 row-cols-md-2 d-grid-row">
     <div class="mb-4">
       <div class="card">
-        <h5 class="card-header">
+        <h5 class="card-header" :style="{'color': uiConfiguration?.primaryTextColor}">
           Contact
         </h5>
         <div class="card-body">
-          <h5 class="card-title">
+          <h5 class="card-title" :style="{'color': uiConfiguration?.primaryTextColor}">
             Contact negotiation support by email.
           </h5>
           <div class="mt-4">
             <a
               href="mailto:negotiator@helpdesk.bbmri-eric.eu"
               class="link-underline link-underline-opacity-0"
-            ><i class="bi bi-envelope-at" /> Email</a>
+              :style="{'color': uiConfiguration?.LinksColor}" 
+            ><i class="bi bi-envelope-at"/> Email</a>
           </div>
         </div>
       </div>
@@ -24,16 +25,21 @@
 
     <div class="mb-4">
       <div class="card">
-        <h5 class="card-header">
+        <h5 class="card-header" :style="{'color': uiConfiguration?.primaryTextColor}">
           Feedback
         </h5>
         <div class="card-body">
-          <h5 class="card-title">
+          <h5 class="card-title" :style="{'color': uiConfiguration?.primaryTextColor}">
             We value your feedback to improve our services.
           </h5>
           <div class="mt-4">
-            <a href="https://forms.office.com/e/Z3EvCnp6cw"><i class="bi bi-box-arrow-up-right" />
-              Feedback form</a>
+            <a 
+              href="https://forms.office.com/e/Z3EvCnp6cw"
+              :style="{'color': uiConfiguration?.LinksColor}" 
+            >
+              <i class="bi bi-box-arrow-up-right" />
+              Feedback form
+            </a>
           </div>
         </div>
       </div>
@@ -42,7 +48,7 @@
 
   <div class="mb-4">
     <div class="card">
-      <h5 class="card-header">
+      <h5 class="card-header" :style="{'color': uiConfiguration?.primaryTextColor}">
         FAQ
       </h5>
       <div class="card-body">
@@ -59,6 +65,7 @@
                 data-bs-target="#collapseOne"
                 aria-expanded="false"
                 aria-controls="collapseOne"
+                :style="{'color': uiConfiguration?.primaryTextColor}"
               >
                 I want direct communication with the biobanks to align my needs with their possibilities.
               </button>
@@ -67,8 +74,9 @@
               id="collapseOne"
               class="accordion-collapse collapse"
               data-bs-parent="#accordionExample"
+              :style="{'color': uiConfiguration?.secondaryTextColor}"
             >
-              <div class="accordion-body text-secondary-text">
+              <div class="accordion-body">
                 BBMRI-ERIC provides its Negotiator as a platform to support researchers and biobankers in the complex communication necessary to achieve a consensus on conditions of samples and/or data delivery. Requests issued by researchers are becoming increasingly complex and biobanks and biomolecular resources also need to verify availability of the data and biological material for a particular purpose defined by the researcher. Negotiator allows effective streamlining and monitoring throughout the whole process of these negotiations.
               </div>
             </div>
@@ -82,6 +90,7 @@
                 data-bs-target="#collapseTwo"
                 aria-expanded="false"
                 aria-controls="collapseTwo"
+                :style="{'color': uiConfiguration?.primaryTextColor}"
               >
                 I want to browse all biobanks in the BBMRI-ERIC.
               </button>
@@ -90,8 +99,9 @@
               id="collapseTwo"
               class="accordion-collapse collapse"
               data-bs-parent="#accordionExample"
+              :style="{'color': uiConfiguration?.secondaryTextColor}"
             >
-              <div class="accordion-body text-secondary-text">
+              <div class="accordion-body">
                 The Directory is a broad catalogue of our biobanks with aggregated information on their collections. You can browse and filter by country, sample type, quality marks, ICD-10 and more.
               </div>
             </div>
@@ -105,6 +115,7 @@
                 data-bs-target="#collapseThree"
                 aria-expanded="false"
                 aria-controls="collapseThree"
+                :style="{'color': uiConfiguration?.primaryTextColor}"
               >
                 I need tissue and liquid samples fitting to my research scope.
               </button>
@@ -113,8 +124,9 @@
               id="collapseThree"
               class="accordion-collapse collapse"
               data-bs-parent="#accordionExample"
+              :style="{'color': uiConfiguration?.secondaryTextColor}"
             >
-              <div class="accordion-body text-secondary-text">
+              <div class="accordion-body">
                 The Locator is your access to the samples and data associated to the samples based on real-time search on donor and sample level.
               </div>
             </div>
@@ -128,6 +140,7 @@
                 data-bs-target="#collapseFour"
                 aria-expanded="false"
                 aria-controls="collapseFour"
+                :style="{'color': uiConfiguration?.primaryTextColor}"
               >
                 I am interested more in genomics and population data analysis than samples themselves.
               </button>
@@ -136,8 +149,9 @@
               id="collapseFour"
               class="accordion-collapse collapse"
               data-bs-parent="#accordionExample"
+              :style="{'color': uiConfiguration?.secondaryTextColor}"
             >
-              <div class="accordion-body text-secondary-text">
+              <div class="accordion-body">
                 The Finder is a data analysis tool focused on clinical, phenotypic and genomics data.
               </div>
             </div>
@@ -147,13 +161,13 @@
     </div>
   </div>
 
-  <h1 class="text-center mt-3 mb-5">
+  <h1 class="text-center mt-3 mb-5" :style="{'color': uiConfiguration?.primaryTextColor}">
     Learn
   </h1>
 
   <div class="row row-cols-1 row-cols-md-2 d-grid-row">
     <div class="mb-4">
-      <div class="card">
+      <div class="card" :style="{'color': uiConfiguration?.primaryTextColor}">
         <h5 class="card-header">
           Learn about negotiatior
         </h5>
@@ -162,14 +176,14 @@
             Learn more about the Negotiator and its use to communicate with BioBanks to obtain information on the availability of relevant samples/data.
           </h5>
           <div class="mt-4">
-            <a href="https://www.bbmri-eric.eu/services/sample-negotiator/"> <i class="bi bi-box-arrow-up-right" /> Go to learn page</a>
+            <a href="https://www.bbmri-eric.eu/services/sample-negotiator/" :style="{'color': uiConfiguration?.LinksColor}" > <i class="bi bi-box-arrow-up-right" /> Go to learn page</a>
           </div>
         </div>
       </div>
     </div>
 
     <div class="mb-4">
-      <div class="card">
+      <div class="card" :style="{'color': uiConfiguration?.primaryTextColor}">
         <h5 class="card-header">
           Federated Platform
         </h5>
@@ -178,13 +192,13 @@
             Information about the Federated Platform and its advanced services such as the federated search or data analysis platform.
           </h5>
           <div class="mt-5" />
-          <a href="https://www.bbmri-eric.eu/federated-platform/"> <i class="bi bi-box-arrow-up-right" /> Go to Federated Platform page</a>
+          <a href="https://www.bbmri-eric.eu/federated-platform/" :style="{'color': uiConfiguration?.LinksColor}" > <i class="bi bi-box-arrow-up-right" /> Go to Federated Platform page</a>
         </div>
       </div>
     </div>
 
     <div class="mb-4">
-      <div class="card">
+      <div class="card" :style="{'color': uiConfiguration?.primaryTextColor}">
         <h5 class="card-header">
           BBMRI-ERIC Sample and Data portal
         </h5>
@@ -193,7 +207,7 @@
             Information about the BBMRI-ERIC access pipeline helping expedite access to samples and data.
           </h5>
           <div class="mt-4" />
-          <a href="https://www.bbmri-eric.eu/bbmri-sample-and-data-portal/"><i class="bi bi-box-arrow-up-right" /> Go to BBMRI-ERIC Sample and Data portal page</a>
+          <a href="https://www.bbmri-eric.eu/bbmri-sample-and-data-portal/" :style="{'color': uiConfiguration?.LinksColor}" ><i class="bi bi-box-arrow-up-right" /> Go to BBMRI-ERIC Sample and Data portal page</a>
         </div>
       </div>
     </div>
@@ -201,15 +215,21 @@
 </template>
 
 <script setup>
-  import { onMounted } from "vue"
+  import { onMounted, computed } from "vue"
   import { useUserStore } from "../store/user"
-  
+  import { useUiConfiguration } from '@/store/uiConfiguration.js'
+
+  const uiConfigurationStore = useUiConfiguration()
   const userStore = useUserStore()
   
   onMounted(async () => {
     if (Object.keys(userStore.userInfo).length === 0) {
       await userStore.retrieveUser()
     }
+  })
+
+  const uiConfiguration = computed(() => {
+    return uiConfigurationStore.uiConfiguration?.theme
   })
 </script>
 
